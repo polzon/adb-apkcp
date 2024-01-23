70509cbcd17a1054c54ba72af357ae654d383ee0
Zack Polson <zack@polson.dev>
2024-01-23 13:24:35 -0700

    still figuring out regex, ran out of time

--- adbcopy.sh
+++ adbcopy.sh
@@ -6,20 +6,19 @@ IFS=$'\n\t'
 
 # All packages relating to google services
 google_services=( \
-    com.google.android.gsf \
+#    com.google.android.gsf \
     com.google.android.gms \
-    com.android.vending \
+#    com.android.vending \
 )
 
 # Pulls the package's apk from the device to the local folder
 pull_apk() {
     for i in $@; do
         apk_path=$(adb shell pm path $i | cut -f 2 -d ":")
-        for j in $apk_path; do
-            # will figure out how to do this tomorrow lol
-            echo $j | grep -e '(?<=split_).+?(?=.apk)'
-        done
-    done;
+        echo $apk_path | grep -Po '(?<=split_).+?(?=\.apk)'
+        #apk_path=[[ "$apk_path" =~ '(?<=split_).*?(?=\.apk)' ]]
+        #echo $apk_path
+    done
     #adb pull $path "$1.apk"
 }
 
