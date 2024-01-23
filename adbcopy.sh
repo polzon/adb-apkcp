8053995f3d1b2d126ca05268493c4d7784c66406
Zack Polson <zack@polson.dev>
2024-01-23 03:05:16 -0700

    trying to figure out how regex works

--- adbcopy.sh
+++ adbcopy.sh
@@ -4,18 +4,20 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# All packages relating to google services
 google_services=( \
     com.google.android.gsf \
     com.google.android.gms \
     com.android.vending \
 )
 
-# Pulls the apk from the device to the local folder
+# Pulls the package's apk from the device to the local folder
 pull_apk() {
     for i in $@; do
         apk_path=$(adb shell pm path $i | cut -f 2 -d ":")
         for j in $apk_path; do
-            echo $j
+            # will figure out how to do this tomorrow lol
+            echo $j | grep -e '(?<=split_).+?(?=.apk)'
         done
     done;
     #adb pull $path "$1.apk"
