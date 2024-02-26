33d9e1c6b00468bf19370bf8f9e4965d81480619
Zack Polson <zack@polson.dev>
2024-02-25 19:06:17 -0700

    temp modification to copy 3rd party work apps

--- adbcopy.sh
+++ adbcopy.sh
@@ -4,20 +4,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+user=10
+
 # All packages relating to google services
 google_services=( \
     com.google.android.gsf \
     com.google.android.gms \
     com.android.vending
 )
-other_services=( \
-    com.airwatch.browser \
-    com.airwatch.tunnel \
-    com.microsoft.office.outlook \
-)
-
-user='12'
-installed_packages=$(adb shell pm list packages | cut -f 2 -d ":")
+work_packages=$(adb shell pm list packages -3 --user 10 | cut -f 2 -d ":")
 
 # Pulls the package's apk from the device to the local folder
 copy_apk() {
@@ -25,27 +20,33 @@ copy_apk() {
     if [[ ! -d $1 ]]; then mkdir $1; fi
     cd $1
     
-    paths=$(adb shell pm path "$1" | cut -f 2 -d ":")
+    paths=$(adb shell pm path --user $user $1 | cut -f 2 -d ":")
     packages=$(echo "$paths" | grep -Po '(?<=split_).+?(?=\.apk)|base')
     
     # Pull apks from devices
     for i in $paths; do adb pull $i; done
     
     # Install packages
-    echo "Installing $1"
-    adb install-multiple --user $user *.apk
-    echo "Installed $1"
+    #echo "Installing $1"
+    #adb install-multiple --user $user *.apk
+    #echo "Installed $1"
     
     # Cleanup
-    rm *.apk
-    cd ..
-    rmdir $1
+    #rm *.apk
+    #cd ..
+    #rmdir $1
     
+    cd ..
     return
 }
 
+if [[ ! -d "apk" ]]; then mkdir 'apk'; fi
+cd apk
+
 # Install defined packages
-for i in ${other_services[@]}; do
-    #copy_apk $i
-    copy_apk com.loudtalks
+for i in ${work_packages[@]}; do
+    #echo $other_services
+    echo "Pulling: $i"
+    copy_apk $i
+    #copy_apks $(adb shell pm list packages -3 --user 10)
 done
